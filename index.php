6bed3b7ecc5ae15bdb2b372cea1daabf2d9ceff3
Ibrohimjon25 <55137101+Ibrohimjon25@users.noreply.github.com>
2020-08-15 13:32:44 +0500

    ornatildi
    dfsdfsdfs

--- index.php
+++ index.php
@@ -1,3 +1,62 @@
 <?php
-  echo "salom bolalar!!!";
-?>
+define('API_KEY', '1368666227:AAEoRSX2XyYGoVXnrjPJrMmpmlXCxS6XtHc');
+$webSite = 'https://api.telegram.org/bot'.API_KEY;
+
+function bot($method, $datas = []){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
+    $res = curl_exec($ch);
+    if (curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+
+function typing($ch){
+    return bot('sendChatAction', [
+        'chat_id'=>$ch,
+        'action'=>'typing'
+    ]);
+}
+//https://api.telegram.org/bot1368666227:AAEoRSX2XyYGoVXnrjPJrMmpmlXCxS6XtHc/sendMessage?chat_id=654142079&text=Salom%20dunyo
+$update = json_decode(file_get_contents($webSite."/getUpdates"), true);
+$message = $update['result'][0]['message'];
+$chat_id = $message['chat']['id'];
+$text = $message['text'];
+
+$button = json_encode([
+    'resize_keyboard'=>true,
+    'keyboard'=>[
+        [['text'=>'Biz haqimizda'], ['text'=>'Manzil']],
+        [['text'=>'Bizning xizmatlar'], ['text'=>'Aloqa']]
+    ]
+]);
+
+if (isset($text)){
+    typing($chat_id);
+}
+
+//if ($text == "/start"){
+//    bot('sendMessage', [
+//        'chat_id'=>$chat_id,
+//        'text'=>'Assalomu alaykum bizning botimizga xush kelibsiz',
+//        'parse_mode'=>'markdown',
+//        'reply_markup'=>$button
+//    ]);
+//}
+
+if ($text == "Manzil"){
+    bot('sendMessage', [
+        'chat_id'=>$chat_id,
+        'text'=>'Nima gaplar',
+        'parse_mode'=>'markdown',
+        'reply_markup'=>$button
+    ]);
+}
+
+
+
